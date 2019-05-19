7c781b125163a5877b9a8ecf73bebaec125effd4
steven victor <chikodi543@gmail.com>
2019-05-19 22:44:50 +0100

    Migrate functionality to create a new card to the deck file

--- main.go
+++ main.go
@@ -1,20 +1,21 @@
 package main
 
-import "fmt"
-
 func main() {
-	// card := "Aces of Spades"
-	// card := newCard()
-	// fmt.Println(card)
-	cards := []string{"Ace of Diamonds", newCard()}
-	cards = append(cards, "Six of Spades")
 
-	// fmt.Println(cards)
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
+	// cards := []string{"Ace of Diamonds", newCard()}
+	// cards := deck{"Ace of Diamonds", newCard()}
+	// cards = append(cards, "Six of Spades")
+
+	// for i, card := range cards {
+	// 	fmt.Println(i, card)
+	// }
+
+	cards := newDeck()
+
+	cards.print()
+
 }
 
-func newCard() string {
-	return "Ace of Diamond"
-}
+// func newCard() string {
+// 	return "Ace of Diamond"
+// }
